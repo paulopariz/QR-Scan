29aa50fa5d60eb382b40a09ad6399bc396358e5e
paulopariz <paulopariz01@gmail.com>
2023-03-31 01:05:10 -0300

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{html,js,vue}"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
