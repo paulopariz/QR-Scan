8edd099dd7d7502919f1296949f46e4b1e2a9274
paulopariz <paulopariz01@gmail.com>
2023-04-08 00:36:45 -0300

    remove modalGenerate

--- src/components/generate-qr/OptionsGenerateQR.vue
+++ src/components/generate-qr/OptionsGenerateQR.vue
@@ -20,16 +20,37 @@
         </div>
       </div>
 
+      <!--MODAL-->
+
       <div v-if="viewModal">
         <BackgroundForModals :closeModal="closeModal" :title="viewModal.name" />
 
-        <ModalGenerateQR
-          :img="viewModal.icon"
-          :placeholder="viewModal.placeholder"
-          :typeInput="viewModal.type"
-          :url="viewModal.url"
-          :label="viewModal.label"
-        />
+        <div
+          class="w-5/6 gap-9 drop-shadow-2xl shadow-lg absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/50 rounded-lg border-2 border-x-0 border-light flex flex-col justify-between items-center px-5 py-8"
+        >
+          <img :src="viewModal.icon" class="w-16 select-none" />
+
+          <div class="flex flex-col gap-3 w-full">
+            <label for="Username" class="text-white-2 tracking-wide">{{
+              viewModal.label
+            }}</label>
+            <input
+              type="viewModal.typeInput"
+              :placeholder="viewModal.placeholder"
+              v-model="username"
+              class="px-4 py-2 bg-[#222121] border border-white-2/70 rounded-md drop-shadow-2xl shadow-lg text-white-2/40 outline-none transition-all focus:border-light"
+            />
+
+            <input type="text" id="twitter-url" class="" :value="url" readonly />
+          </div>
+
+          <button
+            @click="generate"
+            class="mt-4 px-6 py-2 bg-light text-dark tracking-wide rounded-lg drop-shadow-2xl shadow-lg transition-all active:scale-95 hover:scale-95"
+          >
+            Gerar QR Code
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -37,13 +58,13 @@
 
 <script>
 import BackgroundForModals from "../BackgroundForModals.vue";
-import ModalGenerateQR from "./ModalGenerateQR.vue";
+
 export default {
-  components: { BackgroundForModals, ModalGenerateQR },
+  components: { BackgroundForModals },
   data() {
     return {
       viewModal: null,
-      img: require("@/assets/img/iconInstagram.svg"),
+      username: "",
 
       Options: [
         {
@@ -131,7 +152,19 @@ export default {
     };
   },
 
+  computed: {
+    url() {
+      return this.viewModal.url + this.username;
+    },
+  },
+
   methods: {
+    generate() {
+      console.log(this.url);
+
+      this.username = "";
+    },
+
     openModal(optionId) {
       this.viewModal = this.Options.find((option) => option.id === optionId);
       window.history.pushState(null, null, `/generate-qr/${this.viewModal.name}`);
