edbb14205e558adaa75eb8c126102bbd3812be3d
paulopariz <paulopariz01@gmail.com>
2023-04-21 02:23:33 -0300

    remove sectionStart

--- src/components/LoaderPage.vue
+++ src/components/LoaderPage.vue
@@ -7,20 +7,37 @@
         <img src="../assets/img/logoLight.svg" class="select-none" alt="Logo" />
       </div>
 
-      <SectionStart
-        :startClick="start"
-        startText="Iniciar"
-        :contentBtn="contentBtn"
-        :loadingBtn="loadingBtn"
-      />
+      <div class="fixed z-40 bottom-20 w-screen">
+        <div class="flex justify-center flex-col items-center gap-11 w-screen">
+          <h1 class="font-normal tracking-wide w-11/12 text-center max-mn:text-sm">
+            Vá e aproveite nossos recursos gratuitamente e facilite sua vida conosco.
+          </h1>
+          <button
+            @click="startClick"
+            class="bg-dark border border-white-2/5 w-4/5 px-7 h-14 select-none rounded-lg transition-all active:scale-95 hover:scale-95"
+          >
+            <div v-show="contentBtn" class="flex items-center justify-between">
+              <span class="text-transparent text-[0px]">.</span>
+              <h1 class="text-white font-bold tracking-wide text-base">Iniciar</h1>
+              <img src="@/assets/img/arrow.svg" alt="Arrow" />
+            </div>
+
+            <IconLoadingBtn
+              class="flex justify-center items-center border-white-2/20 border-r-white-2"
+              v-show="loadingBtn"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import SectionStart from "./SectionStart.vue";
+import IconLoadingBtn from "./iconLoadingBtn.vue";
+
 export default {
-  components: { SectionStart },
+  components: { IconLoadingBtn },
 
   data() {
     return {
@@ -31,7 +48,7 @@ export default {
     };
   },
   methods: {
-    start() {
+    startClick() {
       document.body.style.overflow = "auto";
 
       this.loadingBtn = true;
